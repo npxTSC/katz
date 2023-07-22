65077dbc0b5026e17ed65895042451debb2d4598
Cherry <sheepareawesome777@gmail.com>
2023-07-22 02:09:36 -0400

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const heap = std.heap;
 const curses = @import("curses.zig");
+const heap = std.heap;
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
@@ -19,38 +19,11 @@ pub fn main() !void {
 
     // Define color pairs
     const pair1 = try curses.ColorPair.init(1, curses.COLOR_RED, curses.COLOR_BLACK);
-    _ = pair1;
-    const pair2 = try curses.ColorPair.init(2, curses.COLOR_GREEN, curses.COLOR_BLACK);
-    _ = pair2;
 
     // Print some text
+    try win.attron(pair1.attr());
     try win.mvaddstr(5, 10, "Hello, colorful world!");
-
-    // Apply attributes and color pair to specific characters
-    // curses.chgat(5, curses.A_BOLD | curses.A_UNDERLINE, 1, null); // Make the first 5 characters bold and underlined using color pair 1
-    // curses.chgat(3, curses.A_BOLD, 2, null); // Make the next 3 characters bold using color pair 2
 
     _ = try win.getch(); // Wait for a key press
     _ = try curses.endwin(); // End curses mode
-
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
