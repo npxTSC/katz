9487fcdad31a47b2c42786979de77ab70f8daeab
Cherry <sheepareawesome777@gmail.com>
2023-07-22 01:41:53 -0400

    curses stuff

---
+++ src/curses.zig
@@ -0,0 +1,127 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("src/cursesflags.h");
+    @cInclude("curses.h");
+});
+
+const Error = error.CursesError;
+
+fn checkError(res: c_int) !c_int {
+    if (res == c.ERR) {
+        return Error;
+    }
+    return res;
+}
+
+pub const KEY_RESIZE: c_int = c.KEY_RESIZE;
+pub const KEY_LEFT: c_int = c.KEY_LEFT;
+pub const KEY_RIGHT: c_int = c.KEY_RIGHT;
+pub const KEY_UP: c_int = c.KEY_UP;
+pub const KEY_DOWN: c_int = c.KEY_DOWN;
+
+pub const Window = struct {
+    win: *c.WINDOW,
+    allocator: std.mem.Allocator,
+
+    // TODO: change more things to Window methods
+
+    pub fn erase(self: Window) !void {
+        _ = try checkError(c.werase(self.win));
+    }
+
+    pub fn mvaddstr(self: Window, y: u16, x: u16, str: []const u8) !void {
+        const cstr: []u8 = try self.allocator.alloc(u8, str.len + 1);
+        defer self.allocator.free(cstr);
+        std.mem.copy(u8, cstr, str);
+        cstr[str.len] = 0;
+        _ = try checkError(c.mvwaddstr(self.win, y, x, cstr.ptr));
+    }
+
+    // TODO: don't use "legacy" functions like getmaxy()?
+    pub fn getmaxy(self: Window) u16 {
+        return @intCast(c.getmaxy(self.win));
+    }
+    pub fn getmaxx(self: Window) u16 {
+        return @intCast(c.getmaxx(self.win));
+    }
+
+    pub fn attron(self: Window, attr: c_int) !void {
+        _ = try checkError(c.wattron(self.win, attr));
+    }
+    pub fn attroff(self: Window, attr: c_int) !void {
+        _ = try checkError(c.wattroff(self.win, attr));
+    }
+
+    pub fn keypad(self: Window, bf: bool) !c_int {
+        return checkError(c.keypad(self.win, bf));
+    }
+
+    pub fn getch(self: Window) !c_int {
+        return checkError(c.wgetch(self.win));
+    }
+};
+
+pub const A_STANDOUT = c.MY_A_STANDOUT;
+
+pub fn initscr(allocator: std.mem.Allocator) !Window {
+    const res = c.initscr();
+    if (@intFromPtr(res) == 0) {
+        return Error;
+    }
+    return Window{ .win = res, .allocator = allocator };
+}
+
+pub fn endwin() !void {
+    _ = try checkError(c.endwin());
+}
+
+pub fn curs_set(visibility: c_int) !c_int {
+    return try checkError(c.curs_set(visibility));
+}
+
+pub fn has_colors() bool {
+    return c.has_colors();
+}
+
+pub fn start_color() !void {
+    _ = try checkError(c.start_color());
+}
+
+const color_pair_attrs = [_]c_int{
+    -1, // 0 doesn't seem to be a valid color pair number, curses returns ERR for
+    // it
+    c.MY_COLOR_PAIR_1,
+    c.MY_COLOR_PAIR_2,
+    c.MY_COLOR_PAIR_3,
+    c.MY_COLOR_PAIR_4,
+    c.MY_COLOR_PAIR_5,
+    c.MY_COLOR_PAIR_6,
+    c.MY_COLOR_PAIR_7,
+    c.MY_COLOR_PAIR_8,
+    c.MY_COLOR_PAIR_9,
+    c.MY_COLOR_PAIR_10,
+};
+
+pub const ColorPair = struct {
+    id: c_short,
+
+    pub fn init(id: c_short, front: c_short, back: c_short) !ColorPair {
+        std.debug.assert(1 <= id and id < color_pair_attrs.len);
+        _ = try checkError(c.init_pair(id, front, back));
+        return ColorPair{ .id = id };
+    }
+
+    pub fn attr(self: ColorPair) c_int {
+        return color_pair_attrs[@intCast(self.id)];
+    }
+};
+
+// listed in init_pair man page
+pub const COLOR_BLACK = c.COLOR_BLACK;
+pub const COLOR_RED = c.COLOR_RED;
+pub const COLOR_GREEN = c.COLOR_GREEN;
+pub const COLOR_YELLOW = c.COLOR_YELLOW;
+pub const COLOR_BLUE = c.COLOR_BLUE;
+pub const COLOR_MAGENTA = c.COLOR_MAGENTA;
+pub const COLOR_CYAN = c.COLOR_CYAN;
+pub const COLOR_WHITE = c.COLOR_WHITE;
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,41 @@
 const std = @import("std");
+const heap = std.heap;
+const curses = @import("curses.zig");
 
 pub fn main() !void {
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const st = gpa.deinit();
+        if (st == .leak) {
+            std.debug.print("leaked (bruh)", .{});
+        }
+    }
+
+    const ally = gpa.allocator();
+
+    // Initialize the curses library
+    const window = try curses.initscr(ally);
+    _ = window;
+    try curses.start_color(); // Enable color support
+
+    // Define color pairs
+    const pair1 = try curses.ColorPair.init(1, curses.COLOR_RED, curses.COLOR_BLACK);
+    _ = pair1;
+    const pair2 = try curses.ColorPair.init(2, curses.COLOR_GREEN, curses.COLOR_BLACK);
+    _ = pair2;
+
+    // Print some text
+    curses.mvprintw(5, 10, "Hello, colorful world!");
+
+    // Apply attributes and color pair to specific characters
+    curses.chgat(5, curses.A_BOLD | curses.A_UNDERLINE, 1, null); // Make the first 5 characters bold and underlined using color pair 1
+    curses.chgat(3, curses.A_BOLD, 2, null); // Make the next 3 characters bold using color pair 2
+
+    curses.refresh(); // Refresh the screen
+
+    curses.getch(); // Wait for a key press
+    curses.endwin(); // End curses mode
+
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
