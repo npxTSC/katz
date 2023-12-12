bd67205a285ef93e3dcac5d347d881cb4f1cd482
zP0 <zP@solarsystem.coffee>
2023-12-12 13:17:35 -0900

    async && channelz

--- srv/src/main.rs
+++ srv/src/main.rs
@@ -2,32 +2,48 @@ use std::collections::HashMap;
 
 use std::io::{Read, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::Arc;
+use tokio;
 use tokio::runtime::Runtime;
+use tokio::sync::Mutex;
 //mod chat_channel;
-const SERVER_PORT: i31 = 9000;
+const SERVER_PORT: i32 = 9000;
 
-fn main() {
+struct Channel {
+    UUID: u32,
+    userz: Vec<u32>,
+    messages: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() {
     //Initilize Server - loading configs
     let listener =
         TcpListener::bind(format!("127.0.0.1:{}", SERVER_PORT)).expect("failed to bind to port");
-    //    let mut connections: HashMapUUID, Connection> = HashMap::new();
-    /*    let mut chat: Chat = {
-        let UUID = "Channel".to_string();
-        let message: String = "cookies <3";
-        let messages = message;
+    let ch = Channel {
+        UUID: 0,
+        userz: vec![0, 1],
+        messages: vec!["I like cookiez".to_string()],
     };
-    */
-
+    let channelz = Arc::new(Mutex::new(ch));
+    let channelz_clone = Arc::clone(&channelz);
     //maybe sockets?
 
     let server_sender_port: String = "9001".to_string();
-    let rt = Runtime::new().unwrap();
+    //    let rt = Runtime::new().unwrap();
+
     let sender = TcpListener::bind(format!("127.0.0.1:{}", server_sender_port))
         .expect("failed to bind port");
-    rt.spawn(sender_fn(sender));
-    rt.spawn(receiver_fn(listener));
+    let sender_stream = listener.try_clone().unwrap();
+    tokio::spawn(async move {
+        sender_fn(sender_stream, channelz_clone).await;
+    });
+    let channelz_clone = Arc::clone(&channelz);
+    tokio::spawn(async move {
+        receiver_fn(listener, channelz_clone).await;
+    });
 }
-async fn receiver_fn(listener: TcpListener) {
+async fn receiver_fn(listener: TcpListener, channelz: Arc<Mutex<Channel>>) {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -41,7 +57,7 @@ async fn receiver_fn(listener: TcpListener) {
     }
 }
 
-async fn sender_fn(sender: TcpListener) {
+async fn sender_fn(sender: TcpListener, channelz: Arc<Mutex<Channel>>) {
     loop {
         for stream in sender.incoming() {
             match stream {
