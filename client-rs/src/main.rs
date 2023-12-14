9b7b4bbe9f9a9931215daefdf811ca404d44fd89
zP0 <zP@solarsystem.coffee>
2023-12-14 10:45:38 -0900

    upgradez

--- client-rs/src/main.rs
+++ client-rs/src/main.rs
@@ -1,4 +1,5 @@
 #[allow(unused_imports)]
+#[allow(unused_variables)]
 use crossterm::{
     execute,
     terminal::{Clear, ClearType},
@@ -20,9 +21,9 @@ async fn main() {
     tokio::spawn(async move {
         send_messagez(server_address.clone()).await;
     });
-    tokio::spawn(async move {
-        receiver_fn(server_address_copy).await;
-    });
+    //    tokio::spawn(async move {
+    //        receiver_fn(server_address_copy).await;
+    //    });
 }
 async fn send_messagez(server_address: String) {
     loop {
@@ -38,8 +39,9 @@ async fn send_messagez(server_address: String) {
                     .write(content.as_bytes())
                     .expect("error: message could not be delivered");
 
-                content = "".to_string();
-                connection_handler(stream.try_clone().unwrap());
+                //content = "".to_string();
+                //                connection_handler(stream.try_clone().unwrap());
+                receiver_fn(stream.try_clone().unwrap()).await;
             }
             Err(a) => {
                 println!("error {}", a);
@@ -47,35 +49,56 @@ async fn send_messagez(server_address: String) {
         }
     }
 }
-async fn receiver_fn(server_address: String) {
-    loop {
-        match TcpStream::connect(server_address.clone()) {
-            Ok(mut stream) => {
-                clear_terminal().await;
+struct ChatChannel {
+    UUID: u32,
+    messages: Vec<String>,
+}
+async fn receiver_fn(mut stream: TcpStream) {
+    //    loop {
+    //      match TcpStream::connect(server_address.clone()) {
+    //        Ok(mut stream) => {
+    clear_terminal().await;
 
-                println!("new connection {}", stream.peer_addr().unwrap());
-                let mut buffer = [0; 4096];
-                match stream.read(&mut buffer) {
-                    Ok(bytes_read) => {
-                        let input = String::from_utf8_lossy(&buffer[..bytes_read]);
-                        println!("$~: {}", input);
-                    }
-                    Err(e) => {
-                        println!("Error handling buffer: {}", e);
-                    }
-                }
-
-                let _ = tokio::time::sleep(tokio::time::Duration::from_secs(5));
-            }
-            Err(a) => {
-                println!("error {}", a);
-            }
+    println!("new connection {}", stream.peer_addr().unwrap());
+    let mut buffer = [0; 4096];
+    match stream.read(&mut buffer) {
+        Ok(bytes_read) => {
+            let input = String::from_utf8_lossy(&buffer[..bytes_read]);
+            println!("$~: {}", input);
+        }
+        Err(e) => {
+            println!("Error handling buffer: {}", e);
         }
     }
+
+    let _ = tokio::time::sleep(tokio::time::Duration::from_secs(4));
+    //      }
+    //    Err(a) => {
+    //           println!("error {}", a);
+    //       }
+    //   }
+    //}
 }
 async fn clear_terminal() {
     let mut stdout = stdout();
-    let _ = execute!(stdout, Clear(ClearType::All));
+    let progress = execute!(stdout, Clear(ClearType::All));
+    match progress {
+        Ok(a) => {
+            println!("terminal cleared");
+            let pro = stdout.flush();
+            match pro {
+                Ok(a) => {
+                    println!("flush completed");
+                }
+                Err(a) => {
+                    println!("error flushing {}", a);
+                }
+            }
+        }
+        Err(a) => {
+            println!("error: {}", a);
+        }
+    }
 }
 //async fn listen(stream: TcpStream) {
 //    let listener = TcpListener::bind("127.0.0.1:4000").expect("failed to bind incoming port");
